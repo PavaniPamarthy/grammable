431b43df95f434bbb5f9f69be0f7c7d69a9fff23
Pavani Pamarthy <pavani.pamarthy52@gmail.com>
2019-08-29 21:20:46 +0000

    Styling the app

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -25,9 +25,11 @@ class GramsController < ApplicationController
 
   def new
     @gram = Gram.new
+    
   end
   
   def index
+    @grams = Gram.all
   end
 
   def show
@@ -54,10 +56,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
 end
